c17f8c3f2c38948e68f71356cce644a95034f2e3
Shivamkumar098798 <shivamkumar098798@gmail.com>
2025-03-20 21:32:31 +0530

    commit

--- java_simple_question/insertionsort.java
+++ java_simple_question/insertionsort.java
@@ -1,20 +1,21 @@
+import java.util.Arrays;
+
 public class insertionsort {
     public static void main(String[] args) {
-        int[] arr = { 12, 11, 13, 5, 6 };
+        int[] arr = {12, 11, 13, 5, 6};
         insertionsortfun(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void insertionsortfun(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int temp = arr[i];
-            for (int j = i - 1; j >= 0; j--) {
-                if (arr[j] > temp) {
-                    arr[j + 1] = arr[j];
-                }else{
-                    break;
-                }
-                arr[j + 1] = temp;
+        for (int i = 1; i < arr.length; i++) { 
+            int temp = arr[i]; 
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j]; 
+                j--;
             }
+            arr[j + 1] = temp; 
         }
     }
 }
